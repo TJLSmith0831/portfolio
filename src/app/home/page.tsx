5101f6ebe005cd4b7c2d90f6d4938be223a78f4e
TJLSmith0831 <tristan.smith@pilytix.com>
2024-03-08 08:43:13 -0600

    Configure /home with homeLinks, CTA, and Profile avatar

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,13 +1,71 @@
-import React from 'react'
+import React from 'react';
 import NavBar from '../standardUtils/NavBar';
-import styles from '@/app/page.module.css'
+import styles from '@/app/page.module.css';
+import homeStyles from '@/app/styles/Home.module.css';
+import { Avatar } from 'antd';
+import { FaInstagram, FaGithub, FaMedium, FaLinkedin } from 'react-icons/fa';
+
+const HomeLinks = () => {
+  return (
+    <div className={homeStyles.homeLinks}>
+      <a
+        href="https://www.instagram.com/tjlsmith0831"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaInstagram color="#4d96ff" size="1.5em" />
+      </a>
+      <a
+        href="https://github.com/TJLSmith0831"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaGithub color="#4d96ff" size="1.5em" />
+      </a>
+      <a
+        href="https://medium.com/@tjlsmith0831"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaMedium color="#4d96ff" size="1.5em" />
+      </a>
+      <a
+        href="https://www.linkedin.com/in/tristan-smith-666614158"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaLinkedin color="#4d96ff" size="1.5em" />
+      </a>
+    </div>
+  );
+};
 
 const Home = () => {
-    return (
+  return (
     <div className={styles.main}>
-        <NavBar />
+      <NavBar />
+      <div className={homeStyles.homeRectangleOuter}>
+        <div className={homeStyles.homeRectangleInner}>
+          <div className={homeStyles.homeBody}>
+            <div className={homeStyles.callToAction}>
+              <p style={{ fontSize: '4.5rem', marginBottom: '5%' }}>
+                LEAD THE LEAGUE
+              </p>
+              <p style={{ fontSize: '2.5rem', marginBottom: '5%' }}>
+                WITH FULL-STACK SOLUTIONS
+              </p>
+              <HomeLinks />
+            </div>
+            <Avatar
+              shape="circle"
+              src="/Profile.jpeg"
+              className={homeStyles.avatarStyle}
+            />
+          </div>
+        </div>
+      </div>
     </div>
-    );
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/app/standardUtils/NavBar.tsx
+++ src/app/standardUtils/NavBar.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 import React from 'react';
 import { Button, Typography } from 'antd';
 import styles from '@/app/styles/NavBar.module.css';
@@ -7,33 +7,36 @@ import { NavButtonProps } from '../types/navigation';
 
 /**
  * NavButton component: Renders a navigation button.
- * 
+ *
  * The button's appearance changes based on the current route, indicating
  * if the button's destination matches the current page.
- * 
+ *
  * Props:
  * - name (string): The text to display on the button.
  * - href (string): The destination path for the button.
  */
 const NavButton: React.FC<NavButtonProps> = ({ name, href }) => {
-  const router = useRouter()
+  const router = useRouter();
   const pathname = usePathname();
-  const selected = pathname === href
+  const selected = pathname === href;
 
   const handleNavigate = () => {
-    router.push(href)
-  }
+    router.push(href);
+  };
 
   return (
-    <Button className={selected ? styles.navButtonActive : styles.navButton} onClick={handleNavigate}>
+    <Button
+      className={selected ? styles.navButtonActive : styles.navButton}
+      onClick={handleNavigate}
+    >
       {name}
     </Button>
-  )
-}
+  );
+};
 
 /**
  * NavBar component: Renders the navigation bar with multiple NavButton components.
- * 
+ *
  * Displays a title and a series of NavButtons for different sections of the site.
  */
 const NavBar = () => {
@@ -41,10 +44,10 @@ const NavBar = () => {
     <div className={styles.navigation}>
       <Typography className={styles.title}>TRISTAN SMITH</Typography>
       <div className={styles.navButtons}>
-        <NavButton name={'Home'} href={'/home'}/>
-        <NavButton name={'About'} href={'/about'}/>
-        <NavButton name={'Blog'} href={'/blog'}/>
-        <NavButton name={'Contact'} href={'/contact'}/>
+        <NavButton name={'HOME'} href={'/home'} />
+        <NavButton name={'ABOUT'} href={'/about'} />
+        <NavButton name={'BLOG'} href={'/blog'} />
+        <NavButton name={'CONTACT'} href={'/contact'} />
       </div>
     </div>
   );
