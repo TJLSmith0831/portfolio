3dede53b445236879f6e0cc5944a4a4851666d92
TJLSmith0831 <tristan.smith@pilytix.com>
2024-03-08 05:18:04 -0600

    Create working NavBar component and types

--- src/app/standardUtils/NavBar.tsx
+++ src/app/standardUtils/NavBar.tsx
@@ -1,28 +1,50 @@
+"use client";
 import React from 'react';
 import { Button, Typography } from 'antd';
 import styles from '@/app/styles/NavBar.module.css';
+import { usePathname, useRouter } from 'next/navigation';
+import { NavButtonProps } from '../types/navigation';
 
+/**
+ * NavButton component: Renders a navigation button.
+ * 
+ * The button's appearance changes based on the current route, indicating
+ * if the button's destination matches the current page.
+ * 
+ * Props:
+ * - name (string): The text to display on the button.
+ * - href (string): The destination path for the button.
+ */
+const NavButton: React.FC<NavButtonProps> = ({ name, href }) => {
+  const router = useRouter()
+  const pathname = usePathname();
+  const selected = pathname === href
+
+  const handleNavigate = () => {
+    router.push(href)
+  }
+
+  return (
+    <Button className={selected ? styles.navButtonActive : styles.navButton} onClick={handleNavigate}>
+      {name}
+    </Button>
+  )
+}
+
+/**
+ * NavBar component: Renders the navigation bar with multiple NavButton components.
+ * 
+ * Displays a title and a series of NavButtons for different sections of the site.
+ */
 const NavBar = () => {
-  // If you need the current route, you can get it using the useRouter hook from Next.js
-  // const router = useRouter();
-  // const currentRoute = router.pathname;
-
   return (
     <div className={styles.navigation}>
       <Typography className={styles.title}>TRISTAN SMITH</Typography>
       <div className={styles.navButtons}>
-        <Button className={styles.navButtonActive} type="link" href="/home">
-          Home
-        </Button>
-        <Button className={styles.navButton} type="link" href="/about">
-          About
-        </Button>
-        <Button className={styles.navButton} type="link" href="/blog">
-          Blog
-        </Button>
-        <Button className={styles.navButton} type="link" href="/contact">
-          Contact
-        </Button>
+        <NavButton name={'Home'} href={'/'}/>
+        <NavButton name={'About'} href={'/about'}/>
+        <NavButton name={'Blog'} href={'/blog'}/>
+        <NavButton name={'Contact'} href={'/contact'}/>
       </div>
     </div>
   );
---
+++ src/app/types/navigation.d.ts
@@ -0,0 +1,4 @@
+export interface NavButtonProps {
+    name: string;
+    href: string;
+}
