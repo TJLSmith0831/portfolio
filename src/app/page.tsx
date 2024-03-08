e83556fd1cf37b03e14777126e926d91f661d971
TJLSmith0831 <tristan.smith@pilytix.com>
2024-03-08 05:18:26 -0600

    Create /about, /blog, and /contact pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './page.module.css';
 import NavBar from './standardUtils/NavBar';
 
-export default function Home() {
+export default function App() {
   return (
     <div className={styles.main}>
       <NavBar />
